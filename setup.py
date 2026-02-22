81bd09bc41fef1e3d188ae55a525c128661a65cc
louisboii747 <louishinchliffe11@gmail.com>
2026-02-22 23:53:17 +0000

    fix: update version to 2.5.0 in HardwareMon.py and setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,22 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="hardwaremon",
-    version="2.4.0",
+    version="2.5.0",
     packages=find_packages(),
     install_requires=[
         "psutil",
         "requests",
-        # etc
+        # other deps
     ],
+    entry_points={
+        "console_scripts": [
+            "hardwaremon=hardwaremon.main:main"
+        ]
+    },
     author="Louis",
     description="Hardware monitoring tool for Linux",
-    long_description=long_description,      # <- THIS
-    long_description_content_type="text/markdown",  # <- VERY IMPORTANT
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/louisboii747/HardwareMon",
     classifiers=[
         "Programming Language :: Python :: 3",
