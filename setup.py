a96a86ff492c51ff215b0cac8a78e54427f05d1e
louisboii747 <louishinchliffe11@gmail.com>
2026-02-22 22:49:55 +0000

    fix: restore versioning and metadata in setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,14 @@
 from setuptools import setup, find_packages
-from hardwaremonLINUX import __version__
 
 setup(
     name="HardwareMon",
-    version=__version__,
-    packages=find_packages(exclude=["test_hardwaremon*"]),
-    install_requires=[
-        "psutil",
-        "requests",
-        # add any other dependencies your project needs
-    ],
-    entry_points={
-        "console_scripts": [
-            "hardwaremon=HardwareMon.main:main",  # adjust if your main file is different
-        ]
-    },
-)
-
-
-setup(
-    name="HardwareMon",
-    version=__version__,
+    version="2.0.0",   # increment with each release
     packages=find_packages(),
     install_requires=[
         "psutil",
         "requests"
     ],
+    author="Louis",
+    description="A hardware monitoring script",
+    url="https://github.com/louisboii747/HardwareMon",
 )
