b30d3b657d1b223af6d1d4cd4f45c1caf7be47da
louisboii747 <louishinchliffe11@gmail.com>
2026-02-23 00:03:19 +0000

    fix: update version to 2.5.3 in HardwareMon.py and setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="hardwaremon",
-    version="2.5.2",
+    version="2.5.3",
     packages=find_packages(),
     install_requires=[
         "psutil",
